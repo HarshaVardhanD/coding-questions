33f0ffef81d9e94accbf9e18172f3b66bf45b373
Harsha Vardhan Dasari <38016038+HarshaVardhanD@users.noreply.github.com>
2021-06-20 16:43:57 +0530

    Update DiceSum.java

--- Accenture/DiceSum/DiceSum.java
+++ Accenture/DiceSum/DiceSum.java
@@ -1,1 +1,13 @@
-
+import java.util.Scanner;
+class DiceSum {
+	public static void main(String[] args) {
+		Scanner input=new Scanner(System.in);
+		int x=input.nextInt(),count=0;
+		input.close();
+		for (int i=1;i<=6;i++) {
+			if ( (x-i)<=6 && (x-i)>=1 ) {
+				count++;
+			}
+		}System.out.println(count);
+	}
+}
