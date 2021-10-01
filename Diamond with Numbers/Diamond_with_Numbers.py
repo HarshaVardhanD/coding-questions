7320f48069e2c8a28bcc4f0177eab68c52f9cebe
Harsha Vardhan Dasari <38016038+HarshaVardhanD@users.noreply.github.com>
2021-10-01 14:07:02 +0530

    Update Diamond_with_Numbers.py

--- Diamond with Numbers/Diamond_with_Numbers.py
+++ Diamond with Numbers/Diamond_with_Numbers.py
@@ -1,1 +1,26 @@
 
+limit=int(input())
+#first half
+for i in range(1,limit+1):
+  for j in range(limit-i):
+    print(end=" ")
+  num=1
+  for number in range(2*i-1):
+    print(end=str(num))
+    if number+1 >= i :
+      num = num-1
+    else:
+      num = num + 1
+  print()
+#second half
+for i in range(1,limit):
+  for j in range(i):
+    print(end=" ")
+  num=1
+  for number in range(2*(limit-i)-1):
+    print(end=str(num))
+    if number  >= i:
+      num = num - 1
+    else:
+      num = num + 1
+  print()
